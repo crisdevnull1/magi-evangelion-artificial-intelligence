afc310e2dd31f34f0470c8f3a1da8bbe34c5e6f5
cristopher <cadasmeq@gmail.com>
2023-05-10 11:42:51 -0400

    test: prueba unitaria agente

--- app/agent.py
+++ app/agent.py
@@ -1,11 +1,38 @@
 import datetime
+import logging
 from typing import Any
 
 import inject
 from langchain.llms import OpenAI
-from prompt_templates import prompt_templates as template
 
+import app.prompts as template
 from app.config import Config
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+
+
+class ResponseProcessor:
+    def __init__(self, llm: OpenAI):
+        self.llm = llm
+
+    def process_response(self, question: str, prompt_template: str) -> str:
+        prompt = prompt_template.format(question=question)
+        response = self.llm(prompt)
+        logger.debug(f"Response: {response}")
+        return response
+
+    def process_scientist_response(self, question: str) -> str:
+        prompt_template = template.scientist_brain
+        return self.process_response(question, prompt_template)
+
+    def process_mother_response(self, question: str) -> str:
+        prompt_template = template.mother_brain
+        return self.process_response(question, prompt_template)
+
+    def process_woman_response(self, question: str) -> str:
+        prompt_template = template.woman_brain
+        return self.process_response(question, prompt_template)
 
 
 class Agent:
@@ -14,52 +41,41 @@ class Agent:
     def __init__(self):
         self.llm = OpenAI(
             openai_api_key=self.config.OPENAI_API_KEY,
-            model_name="gpt-4",
+            model_name="gpt-3.5-turbo",
             temperature=1,
         )
         self.summarize_llm = OpenAI(
             openai_api_key=self.config.OPENAI_API_KEY,
-            model_name="gpt-4",
+            model_name="gpt-3.5-turbo",
             temperature=0,
         )
-
-    def process_as_scientist(self, question: str) -> str:
-        scientist_prompt = template.scientist_brain.format(question=question)
-        scientist_response = self.llm(scientist_prompt)
-        return scientist_response
-
-    def process_as_mother(self, question: str) -> str:
-        mother_prompt = template.mother_brain.format(question=question)
-        mother_response = self.llm(mother_prompt)
-        return mother_response
-
-    def process_as_woman(self, question: str) -> str:
-        woman_prompt = template.woman_brain.format(question=question)
-        woman_response = self.llm(woman_prompt)
-        return woman_response
+        self.response_processor = ResponseProcessor(self.llm)
 
     def summarize_question(self, responses: Any) -> str:
         summary = self.summarize_llm(template.summarize_answers.format(text=responses))
         return summary
 
     def ask(self, question: str):
-        scientist_response = self.process_as_scientist(question)
-        mother_response = self.process_as_mother(question)
-        woman_response = self.process_as_woman(question)
+        scientist_response = self.response_processor.process_scientist_response(
+            question
+        )
+        mother_response = self.response_processor.process_mother_response(question)
+        woman_response = self.response_processor.process_woman_response(question)
 
         summary = self.summarize_question(
             [scientist_response, mother_response, woman_response]
         )
+        logger.debug(f"Summary: {summary}")
+        if self.config.DEBUG:
+            current_date = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+            log_file_name = f"logs/execution_{current_date}.txt"
 
-        current_date = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
-        log_file_name = f"summary_log_{current_date}.txt"
-
-        with open(log_file_name, "w+") as f:
-            f.write(f"Fecha: {current_date}\n")
-            f.write(f"Pregunta: {question}\n\n")
-            f.write(f"Respuesta Cientifica: {scientist_response}\n\n")
-            f.write(f"Respuesta Madre: {mother_response}\n\n")
-            f.write(f"Respuesta Mujer: {woman_response}\n\n")
-            f.write(f"Resumen: {summary}\n")
+            with open(log_file_name, "w+") as f:
+                f.write(f"Fecha: {current_date}\n")
+                f.write(f"Pregunta: {question}\n")
+                f.write(f"Respuesta Cientifica: {scientist_response}\n")
+                f.write(f"Respuesta Madre: {mother_response}\n")
+                f.write(f"Respuesta Mujer: {woman_response}\n")
+                f.write(f"Resumen: {summary}\n")
 
         return summary
--- app/config.py
+++ app/config.py
@@ -17,9 +17,7 @@ except ImportError:
 class Config:
     def __init__(self):
         self.OPENAI_API_KEY = os.environ["OPENAI_API_KEY"]
-        self.PINECONE_API_KEY = os.environ["PINECONE_API_KEY"]
-        self.PINECONE_ENVIRONMENT = os.environ["PINECONE_ENVIRONMENT"]
-        self.PINECONE_INDEX = os.environ["PINECONE_INDEX"]
+        self.DEBUG = os.environ["DEBUG"] or False
 
 
 def configure(binder):
